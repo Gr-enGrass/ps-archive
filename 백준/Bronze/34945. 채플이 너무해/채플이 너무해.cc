05de3482bdbed1c65eb2c405602c688db6938331
GreenGrass <spatiumdog@gmail.com>
2026-04-15 23:03:28 +0900

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cc
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    if(n<6)
+        cout<< "Oh My God!";
+    else
+        cout << "Success!";
+
+    return 0;
+}
