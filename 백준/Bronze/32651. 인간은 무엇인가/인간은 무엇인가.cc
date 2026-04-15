6bacb68f3db000cb81ab1b7b3a01fceb21a1bed8
GreenGrass <spatiumdog@gmail.com>
2026-04-16 00:04:02 +0900

    [Bronze IV] Title: 인간은 무엇인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32651. 인간은 무엇인가/인간은 무엇인가.cc
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if(n<=100000&&n%2024==0)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+
+    return 0;
+}
