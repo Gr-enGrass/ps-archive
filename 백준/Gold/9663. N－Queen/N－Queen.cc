8eee5c3569316bc21281458baad5aad46260b0af
GreenGrass <spatiumdog@gmail.com>
2026-04-16 00:25:25 +0900

    [Gold IV] Title: N-Queen, Time: 1760 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,32 +1,42 @@
+
 #include <iostream>
+
 using namespace std;
 
 int n;
-int cnt = 0;
+int cnt;
+bool isUsed1[15];//세로 확인 
+bool isUsed2[29];//대각선 확인(우측상단-좌측하단)
+bool isUsed3[29];//대각선 확인(좌측상단-우측하단)
 
-void solve(int cols, int diag1, int diag2) {
-    if (cols == (1 << n) - 1) 
-    { // 모든 퀸 배치 완료
-        cnt++;
-        return;
-    }
-
-    // 현재 행에서 놓을 수 있는 위치 계산 ,상위비트 배제
-    int available = ~(cols | diag1 | diag2) & ((1 << n) - 1);
-
-    while (available) 
+int ang(int x)//x번째 행 부터
+{
+    if(x==n)
     {
-        int pos = available & -available; // 가장 오른쪽 1비트 선택
-        available -= pos;
-
-        // 퀸 놓고 재귀
-        solve(cols | pos, (diag1 | pos) << 1, (diag2 | pos) >> 1);
+        cnt++;
     }
+    
+    for(int i = 0;i<n;i++)//x행의 i 번째 칸
+    {
+        if(isUsed1[i]||isUsed2[i+x]||isUsed3[x-i+n-1])
+        {
+            continue;
+        }
+        isUsed1[i] = true;
+        isUsed2[i+x] = true;
+        isUsed3[x-i+n-1] = true;
+        ang(x+1);
+        isUsed1[i] = false;
+        isUsed2[i+x] = false;
+        isUsed3[x-i+n-1] = false;
+    }
+    return cnt;
 }
 
-int main() {
+int main()
+{
     cin >> n;
-    solve(0, 0, 0); // 초기값
-    cout << cnt << endl;
+    ang(0);
+    cout << cnt;
     return 0;
 }
