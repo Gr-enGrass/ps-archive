c49e593dc6c018499442d715782c3b243d059afe
GreenGrass <spatiumdog@gmail.com>
2026-04-16 00:58:23 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    
+    if(a+c<b+d)
+    {
+        cout << "Hanyang Univ.";
+    }
+    else if(a+c>b+d)
+    {
+        cout << "Yongdap";
+    }
+    else
+    {
+        cout << "Either";
+    }
+}
