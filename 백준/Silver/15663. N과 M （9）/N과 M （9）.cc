056681bf893996d88435cfa641f3e1c936f4ad5b
GreenGrass <spatiumdog@gmail.com>
2026-04-16 00:23:59 +0900

    [Silver II] Title: N과 M (9), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n,m;
+int arr[8];
+bool isUsed[8];
+int result[8];
+
+
+void ang(int x)
+{
+    if(x==m)
+    {
+        for(int i =0;i<m;i++)
+        {
+            cout << result[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    
+    int temp=0;
+    for(int i = 0;i<n;i++)
+    {
+        if(!isUsed[i]&&temp!=arr[i])
+        {
+            temp = arr[i];
+            result[x]=arr[i];
+            isUsed[i]=true;
+            ang(x+1);
+            isUsed[i]=false;
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    for(int i =0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr,arr+n);
+    ang(0);
+
+    return 0;
+}
