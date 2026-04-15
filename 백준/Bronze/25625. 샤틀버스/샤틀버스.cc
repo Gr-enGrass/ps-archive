262e767852137b319d77a3e8b4fac7578cd061ff
GreenGrass <spatiumdog@gmail.com>
2026-04-16 00:01:23 +0900

    [Bronze IV] Title: 샤틀버스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.cc
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x ,y;
+    cin >> x >> y;
+    
+    if(x<y)
+    {
+        cout << y - x;
+    }
+    else
+    {
+        cout << x + y;
+    }
+
+    return 0;
+}
