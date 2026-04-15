ae8ecfee0dd0baa699baf1c6bfe94c051433081e
GreenGrass <spatiumdog@gmail.com>
2026-04-15 22:09:27 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string str = "I love DGU";
+
+    while (n--)
+    {
+        cout << str << '\n';
+    }
+}
