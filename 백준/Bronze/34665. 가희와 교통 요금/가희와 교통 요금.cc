5479ae50c4771b310bce38e5c0ce47e0015ba989
GreenGrass <spatiumdog@gmail.com>
2026-04-15 23:47:55 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.cc
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string a,b;
+    cin >> a >> b;
+    
+    if(a==b)
+    {
+        cout << 0;
+    }
+    else
+    {
+        cout << 1550;
+    }
+    return 0;
+}
