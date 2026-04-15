02bbb0cef9219f1349f015d044fa73c81a688ba5
GreenGrass <spatiumdog@gmail.com>
2026-04-15 21:54:43 +0900

    [Silver V] Title: 중간고사 채점, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15702. 중간고사 채점/중간고사 채점.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <limits>
+using namespace std;
+
+bool cmp(const pair<int,int>&a, const pair<int,int>&b)
+{
+	if (a.second == b.second)
+	{
+		return a.first < b.first;
+	}
+	return a.second > b.second;
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, m;
+	cin >> n >> m;
+
+	vector<int>score;
+	vector<pair<int, int>>result;
+	for (int i = 0;i < n;i++)
+	{
+		int temp;
+		cin >> temp;
+		score.push_back(temp);
+	}
+	while (m--)
+	{
+		int temp;
+		cin >> temp;
+
+		int sum = 0;
+		for (int i = 0;i < n;i++)
+		{
+			char c;
+			cin >> c;
+			if (c == 'O')
+			{
+				sum += score[i];
+			}
+		}
+		result.push_back(make_pair(temp,sum));
+	}
+	sort(result.begin(),result.end(),cmp);
+	cout << result.front().first << ' ' << result.front().second;
+}
