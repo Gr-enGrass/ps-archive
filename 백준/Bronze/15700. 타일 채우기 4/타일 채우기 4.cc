6027d6fe1d788a8f118d94a20ce71a11023bca3b
GreenGrass <spatiumdog@gmail.com>
2026-04-16 00:01:40 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int n,m;
+    cin >> n >>m;
+    
+    long long int result = n * m /2;
+
+    
+    cout << result;
+
+    return 0;
+}
