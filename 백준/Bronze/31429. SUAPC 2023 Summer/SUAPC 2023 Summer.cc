2f21439ac0d7ca1469ada4df7c259744df10320f
GreenGrass <spatiumdog@gmail.com>
2026-04-16 00:32:35 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int N;
+
+    int problems[] = {12, 11, 11, 10, 9, 9, 9, 8, 7, 6, 6};
+    int penalties[] = {1600, 894, 1327, 1311, 1004, 1178, 1357, 837, 1055, 556, 773};
+
+    cin >> N;
+
+    cout << problems[N-1] << " " << penalties[N-1];
+    return 0;
+}
