8e05ab83b9f761080c3a664b40168a5d1a4f2244
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:15:25 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cc
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    
+    cin >> a >> b;
+    
+    if(b<a)
+    {
+        cout << "flight";
+    }
+    else
+    {
+        cout << "high speed rail";
+    }
+
+    return 0;
+}
