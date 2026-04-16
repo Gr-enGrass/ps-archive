1028a997d3d81fb10be46219e4a430d19947ffff
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:39:58 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	char str[21];
+	scanf("%s", str);
+	printf(":fan::fan::fan:\n");
+	printf(":fan::%s::fan:\n", str); 
+	printf(":fan::fan::fan:");
+}	
