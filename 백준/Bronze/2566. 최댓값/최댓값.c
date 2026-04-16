33efdc1514c3fc27ecf7a8122f9bdfa3aadc0700
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:30:27 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.c
+++ 백준/Bronze/2566. 최댓값/최댓값.c
@@ -1,26 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-	int arr[9][9] = { 0 };
-	int max = 0;
-	int x, y;
-
+	int arr[9][9];
 	for (int i = 0; i < 9; i++)
 	{
 		for (int j = 0; j < 9; j++)
 		{
 			scanf("%d", &arr[i][j]);
-			if (max <= arr[i][j])
+		}
+	 }
+	int x=1, y=1;
+	int max = arr[0][0];
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			if (arr[i][j] > max)
 			{
 				max = arr[i][j];
 				x = i;
 				y = j;
 			}
-		}
+		}	
 	}
+
 	printf("%d\n", max);
-	printf("%d %d", x+1, y+1);
+	printf("%d %d", x + 1, y + 1);
 }
