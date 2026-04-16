243b56070e06b94e6bdc51bd18bbc20387862b3a
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:52:54 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.c
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.c
@@ -1,37 +1,35 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-	int N, M;
-	int i, j;
+    int N, M;
+    int i, j;
 
-	int* arr;
-	scanf("%d %d", &N,&M);
-	arr = (int*)malloc(sizeof(int) * N);
+    int* arr;
+    scanf("%d %d", &N, &M);
+    arr = (int*)malloc(sizeof(int) * N);
 
-	for (i = 0; i < N; ++i)
-		arr[i] = i+1;
-	
-	int temp;
+    for (i = 0; i < N; ++i)
+        arr[i] = i + 1;
 
-	for (int I = 0; I < M; ++I)
-	{
-		scanf("%d %d", &i, &j);
-	
-		temp = arr[i-1];
-		arr[i-1] = arr[j-1];
-		arr[j-1] = temp;
-	}
-	for (int Z = 0; Z < N; ++Z)
-		printf("%d ", arr[Z]);
+    int temp;
 
-	
-	
-	free(arr);
-	return 0;
+    for (int I = 0; I < M; ++I)
+    {
+        scanf("%d %d", &i, &j);
+
+        for (int K = 0; K < 1; ++K)
+        {
+            temp = arr[i-1];
+            arr[i-1] = arr[j-1];
+            arr[j-1] = temp;
+        }
+    }
+
+    for (int i = 0; i < N; ++i)
+        printf("%d ", arr[i]);
+
+    free(arr);
+    return 0;
 }
-
-
-
