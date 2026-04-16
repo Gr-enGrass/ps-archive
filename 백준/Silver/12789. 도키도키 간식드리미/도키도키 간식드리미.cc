aa75e6f24d214d7ae8fde5c3d6f1d8d7a15afdf7
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:18:08 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
@@ -1,39 +1,62 @@
 #include <iostream>
+#include <algorithm>
 #include <stack>
 
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
-    cin >> n;
-
-    int arr[1000] = {0};
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
+    
     int check = 1;
+    
     stack<int> line;
-
-    for (int j = 0; j < n; j++) {
-        // 현재 스택의 꼭대기 요소가 check와 같은 경우 pop
-        while (!line.empty() && line.top() == check) {
-            line.pop();
-            check++;
-        }
-
-        if (arr[j] == check) {
-            check++;
-        } else {
-            if (line.empty() || line.top() > arr[j]) {
+    
+    short flag = 1;
+    
+    int arr[1000]={0};
+    
+    
+    
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    for(int j=0;j<n;j++)
+    {
+        if (arr[j]!=check)
+        {
+            if(line.empty()||line.top()>arr[j])
+            {
                 line.push(arr[j]);
-            } else {
-                cout << "Sad";
-                return 0;  // Sad인 경우 조기 종료
+            }
+            else
+            {
+                if(line.empty())
+                {
+                    continue;
+                }
+                else 
+                {
+                    flag=0;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            check++;
+            {
+                while(!line.empty()&&line.top()==check)
+                {
+                    line.pop();
+                    check++;
+                }
             }
         }
     }
-
-    cout << "Nice";
-    return 0;
+    flag ? cout << "Nice" : cout << "Sad";
 }
+     
