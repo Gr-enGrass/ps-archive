fabfe149bf90a796660a3963d7e1aef3bd1b5f2d
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:11:12 +0900

    [Bronze IV] Title: 2033년 밈 투표, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string str;
+    string check[7] = {
+        "Never gonna give you up",
+        "Never gonna let you down",
+        "Never gonna run around and desert you",
+        "Never gonna make you cry",
+        "Never gonna say goodbye",
+        "Never gonna tell a lie and hurt you",
+        "Never gonna stop"
+    };
+    int n;
+    bool same = false;
+    cin >> n;
+    cin.ignore();
+    while(n--)
+    {
+        getline(cin,str);
+        for(int i=0;i<7;i++)
+        {
+            if(str.compare(check[i])==0)
+                same = true;
+        }
+        if(!same)
+        {
+            cout << "Yes";
+            return 0;
+        }
+        same=false;
+    }
+    cout << "No";
+}
