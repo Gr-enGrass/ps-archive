16ae7832d79c048dd8e3a972462e2a2555075f3c
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:41:53 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int k1=1, q1=1, l1=2, b1=2, n1=2, p1=8;
+	int k, q, l, b, n, p;
+	scanf("%d %d %d %d %d %d", &k, &q, &l, &b, &n, &p);
+
+	k1 = k1 - k;
+	q1 = q1 - q;
+	l1 = l1 - l;
+	b1 = b1 - b;
+	n1 = n1 - n;;
+	p1 = p1 - p;
+
+	printf("%d %d %d %d %d %d", k1, q1, l1, b1, n1, p1);
+}
