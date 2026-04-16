73e7dbfaf5d6b02c1ba5741a396ca43d721eaf01
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:24:24 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 428 ms, Memory: 8808 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int sorted[1000001];
+
+void merge(int list[], int left, int mid, int right)
+{
+	int i, j, k, l;
+	i = left;
+	j = mid + 1;
+	k = left;
+
+	while (i <= mid && j <= right)
+	{
+		if (list[i] <= list[j])
+		{
+			sorted[k++] = list[i++];
+		}
+		else
+		{
+			sorted[k++] = list[j++];
+		}
+	}
+
+	if (i > mid)
+	{
+		for (l = j; l <= right; l++)
+		{
+			sorted[k++] = list[l];
+		}
+	}
+	else
+	{
+		for (l = i; l <= mid; l++)
+		{
+			sorted[k++] = list[l];
+		}
+	}
+
+	for (l = left; l <= right; l++)
+	{
+		list[l] = sorted[l];
+	}
+}
+
+void merge_sort(int list[], int left, int right)
+{
+	int mid;
+	if (left < right)
+	{
+		mid = left + (right - left) / 2;
+		merge_sort(list, left, mid);
+		merge_sort(list, mid + 1, right);
+		merge(list, left, mid, right);
+	}
+}
+
+int main()
+{
+	int n;
+	int num[1000001];
+	scanf("%d", &n);
+	for (int i =0;i<n;i++)
+	{
+		scanf("%d", &num[i]);
+	}
+	merge_sort(num, 0, n - 1);
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d\n", num[i]);
+	}
+}
