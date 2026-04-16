41dfe761ba1226fa0a69f84993c5d204cb17a85d
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:09:32 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	stack<int> s;
+	cin >> n;
+	while (n--)
+	{
+		string str = "";
+		int x;
+		
+		
+		cin >> str;
+		
+		switch (str[0])
+		{
+			case 'p':
+				if (str[1] == 'u')
+				{
+					cin >> x;
+					s.push(x);
+				}
+				else
+				{
+					if (s.empty())
+					{
+						cout << "-1\n";
+					}
+					else
+					{
+						cout << s.top() << '\n';
+						s.pop();
+					}
+				}
+				break;
+			case 's':
+				cout << s.size() << '\n';
+				break;
+			case 'e':
+				cout << (s.empty() ? "1\n" : "0\n");
+				break;
+			case 't':
+				if (s.empty())
+				{
+					cout << "-1\n";
+				}
+				else
+				{
+					cout << s.top() << '\n';
+				}
+				break;
+		}
+	}
+}
