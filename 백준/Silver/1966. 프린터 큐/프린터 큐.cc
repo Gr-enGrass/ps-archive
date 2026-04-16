16c535cdd7679cbed8dd6912e1644cb7a430e807
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:13:37 +0900

    [Silver III] Title: 프린터 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.cc
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.cc
@@ -1,52 +1,49 @@
-
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    
-    int m;
-    int n;
-    
-    for(int i=0;i<t;i++)
-    {
-        int cnt=0;
-        cin >> n >> m;
-        queue<pair<int,int>> que; //문서 ,중요도
-        priority_queue<int> pq;
-        for(int j=0;j<n;j++)
-        {
-            int temp;
-            cin >> temp;
-            que.push({j,temp});
-            pq.push(temp);
-        }
-        
-        while(!que.empty())
-        {
-            int idx=que.front().first;
-            int priority=que.front().second;
-            que.pop();
-            
-            if(pq.top()==priority)//중요도 일치
-            {
-                pq.pop();
-                cnt++;
-                
-                if(idx==m)
-                {
-                    
-                    cout << cnt << "\n";
-                    break;
-                }
-            }
-            else
-                que.push({idx,priority});
-        }
-    }
+	int testn;
+	int n, m;
+	int importance;
+	int count;
 
-    return 0;
+	scanf("%d", &testn);
+
+	for (int i = 0; i < testn; i++) {
+		count = 0;
+		scanf("%d %d", &n, &m);
+		
+		priority_queue<int> pq; // 내림차순 정렬해야 pop할 때 높은게 빠져나옴.
+		queue<pair<int, int>> q;
+
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &importance);
+			q.push({ j, importance });
+			pq.push(importance);
+		}
+
+		while (!q.empty()) {
+			// 위치값과, 우선순위 값을 가져온 뒤 pop수행
+			int location = q.front().first;
+			int value = q.front().second;
+			q.pop();
+            
+			// 값 비교
+			if (pq.top() == value) {
+				pq.pop();
+				++count;
+				if (m == location) {
+					printf("%d\n", count);
+					break;
+				}
+			}
+
+			else q.push({ location, value });
+		}
+	}
+
+	return 0;
 }
