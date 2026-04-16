7b186db7f602ccb3c6899bf63d21531f73e02263
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:38:57 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int Ur, Tr, Uo, To;
+	scanf("%d %d %d %d", &Ur, &Tr, &Uo, &To);
+	printf("%d", 56 * Ur + 24 * Tr + 14 * Uo + 6 * To);
+}
