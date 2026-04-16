3545f3d44af2ba670d3fcdccb3d38852f1bca688
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:17:18 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a,b;
+    cin >> a >> b;
+    if(a>=b/2.0)
+    {
+        cout << "E";
+    }
+    else
+    {
+        cout << "H";
+    }
+    return 0;
+}
