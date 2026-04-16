954c100c22d795adf0c92ea5f90bb249cda9b080
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:23:45 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 64 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -1,77 +1,67 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
-	int x;
-	int y;
+    int x;
+    int y;
 } point;
-point sorted[100000];
 
-void merge(point* arr, int left, int mid, int right)
+point sort[100001];
+
+void merge(point* arr, int first, int mid, int last)
 {
-	int i, j, k, l;
-	i = left;
-	j = mid + 1;
-	k = left;
+    int i, j, k;
+    i = first;
+    j = mid + 1;
+    k = first;
 
-	while (i <= mid && j <= right)
-	{
-		if (arr[i].x < arr[j].x)
-			sorted[k++] = arr[i++];
-		else if (arr[i].x > arr[j].x)
-			sorted[k++] = arr[j++];
-		else {
-			if (arr[i].y < arr[j].y)
-				sorted[k++] = arr[i++];
-			else if (arr[i].y > arr[j].y)
-				sorted[k++] = arr[j++];
-		}
-	}
+    while (i <= mid && j <= last) {
+        if (arr[i].x < arr[j].x)
+            sort[k++] = arr[i++];
+        else if (arr[i].x > arr[j].x)
+            sort[k++] = arr[j++];
+        else {
+            if (arr[i].y < arr[j].y)
+                sort[k++] = arr[i++];
+            else if (arr[i].y > arr[j].y)
+                sort[k++] = arr[j++];
+        }
+    }
 
-	if (i > mid)
-	{
-		for (l = j; l <= right; l++)
-			sorted[k++] = arr[l];
-	}
-	else
-	{
-		for (l = i; l <= mid; l++)
-			sorted[k++] = arr[l];
-	}
-
-	for (l = left; l <= right; l++)
-	{
-		arr[l] = sorted[l];
-	}
+    if (i <= mid) {
+        while (i <= mid) 
+            sort[k++] = arr[i++];
+    }
+    else {
+        while (j <= last) 
+            sort[k++] = arr[j++];
+    }
+    for (k = first; k <= last; k++)
+        arr[k] = sort[k];
 }
 
-void merge_sort(point* arr, int left, int right)
-{
-	int mid;
-
-	if (left < right)
-	{
-		mid = (left + right) / 2;
-		merge_sort(arr, left, mid);
-		merge_sort(arr, mid + 1, right);
-		merge(arr, left, mid, right);
-	}
+void mergesort(point* arr, int first, int last) {
+    int mid;
+    if (first < last) {
+        mid = (first + last) / 2;
+        mergesort(arr, first, mid);
+        mergesort(arr, mid + 1, last);
+        merge(arr, first, mid, last);
+    }
 }
 
-int main()
+int main(void)
 {
-	int n;
-	scanf("%d", &n);
-	point arr[100000];
-	for (int i = 0; i < n; i++)
-	{
-		scanf("%d %d", &arr[i].x, &arr[i].y);
-	}
-	merge_sort(arr, 0, n - 1);
-	for (int i = 0; i < n; i++)
-	{
-		printf("%d %d\n", arr[i].x, arr[i].y);
-	}
+    int n;
+    scanf("%d", &n);
 
-	return 0;
+    point* arr = (point*)malloc(sizeof(point)*n);
+    for(int i = 0; i < n; i++)
+        scanf("%d %d", &arr[i].x, &arr[i].y);
+    
+    mergesort(arr, 0, n-1);
+    for (int i = 0; i < n; i++)
+        printf("%d %d\n", arr[i].x, arr[i].y);
+
+    return 0;
 }
