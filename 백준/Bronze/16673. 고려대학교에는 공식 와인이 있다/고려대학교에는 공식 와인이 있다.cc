ba9d64f520bff2433ab517d817af66a499ba7a4a
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:16:23 +0900

    [Bronze III] Title: 고려대학교에는 공식 와인이 있다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16673. 고려대학교에는 공식 와인이 있다/고려대학교에는 공식 와인이 있다.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c,k,p;
+    cin >> c >> k >> p;
+    
+    int wine=0;
+    
+    for(int i =1;i<=c;i++)
+    {
+        wine+=(k*i+p*i*i);
+    }
+    cout << wine;
+    return 0;
+}
