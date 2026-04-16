6a8770c599e0e763d153da99d2cc08a26f114502
GreenGrass <spatiumdog@gmail.com>
2026-04-16 09:54:34 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int T, A, B,Sum;
+	scanf("%d", &T);
+	for (int i = 1; i -1< T; ++i)
+	{
+		scanf("%d %d", &A, &B);
+		Sum = A + B;
+		printf("Case #%d: %d\n", i,Sum);
+	}
+		
+
+	return 0;
+}
