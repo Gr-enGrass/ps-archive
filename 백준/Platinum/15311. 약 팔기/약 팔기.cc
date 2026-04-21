3ab53f10d061f170dff676298be21028fef635e2
GreenGrass <spatiumdog@gmail.com>
2026-04-21 18:11:50 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << 1999 << '\n';
+	for (int i = 0;i < 999;i++)
+	{
+		cout << 1 << ' ';
+	}
+	for (int i = 0;i < 1000;i++)
+	{
+		cout << 1000 << ' ';
+	}
+}
