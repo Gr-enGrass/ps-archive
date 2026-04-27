7316101bdc0d4e2a646211531d275a1e7dbc3326
GreenGrass <spatiumdog@gmail.com>
2026-04-27 21:21:51 +0900

    [Platinum V] Title: 가짜 소수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/13319. 가짜 소수/가짜 소수.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <queue>
+#include <map>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << "1299963601 601";
+}
